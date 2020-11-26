80d551d20c20771711df3f6fd5c194af2a6f4671
Venus Muongsouvanh <vmuongso@uwo.ca>
2020-11-26 15:55:38 -0500

    first commit

--- src/Analysis.java
+++ src/Analysis.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Analysis {
+import java.util.ArrayList;
+
+public class Analysis {
+    String selectedAnalysis;
+    ArrayList<String> countries = new ArrayList<String>();
+    ArrayList<Double> numeratorData = new ArrayList<Double>();
+
+    Analysis(String selectedAnalysis,ArrayList<String> countries ){
+        this.selectedAnalysis = selectedAnalysis;
+        this.countries = countries;
+    }
+    public ArrayList<Double> getAnalysisData(){
+        if (selectedAnalysis == "Total Confirmed Cases per Country"){
+            RetrieveData analysisData = new RetrieveData();
+            for (int i = 0; i < countries.size(); i++) {
+                numeratorData.add(analysisData.getTotalConfirmedCases(countries.get(i)));
+                System.out.println(countries.get(i) + ":" + numeratorData.get(i));
+            }
+        }
+        else if (selectedAnalysis == "Total Deaths per Country"){
+            RetrieveData analysisData = new RetrieveData();
+            for (int i = 0; i < countries.size(); i++) {
+                numeratorData.add(analysisData.getTotalDeathCases(countries.get(i)));
+                System.out.println(countries.get(i) + ":" + numeratorData.get(i));
+            }
+        }
+        return numeratorData;
+    }
 }
